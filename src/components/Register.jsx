861582f681d80b89f88f4697fc1d5fc2387e2635
Kaleef <Kaleef.n13@gmail.com>
2022-12-09 11:21:42 -0500

    Added Min User and pass length

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,31 +2,58 @@ import {React, useState} from "react";
 import { registerUser } from "../api/auth";
 
 //make a request to stranger things API
-//had to pass  setToken to Register or else it will not pull the new state of token 
-//and therefore you can not use it to 
+// had to pass setToken to Register or else it will not pull the new state of token
+// when we submit - we want something to happen ...
+
+//if token, do this
+// if no token, do this...
 const Register = ({setToken}) => {
+    // hit sumit - run register user which will pull out a token..
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    if (username.length > 2)
+    {
+    console.log(`username length is ${username.length}`);
+    }
+    if (password.length > 2)
+    {
+    console.log(`password length is ${password.length}`);
+    }
+    {
     return <div>
-        <form onSubmit={async (e) =>{
-           try {
-            e.preventDefault();
-            //changed the response to token as it was changed form the auth.js 
-            const token = await registerUser(username, password);
-            // setting token into global storage so it can be pulled throughout the app
-           setToken(token);
-           localStorage.setItem("token", token);
-           //console.log("this is token in register", token)
-           } catch (error) {
-            console.error(error)
-           }
-        }}>
-            <input value={username} minLength={5} type='text' placeholder="username" onChange={(e)=>setUsername(e.target.value)}></input>
-            <input value={password} minLength={5} type='password' placeholder='password' onChange={(e)=>setPassword(e.target.value)}></input>
-            <button type="submit">submit</button>
-        </form>
-    </div>
-}
+            
+                <form onSubmit={async (e) =>{
+        
+                    if (password.length > 2 && username.length > 2) {
 
+                try {        
+                e.preventDefault();
+                //changed the response to token as it was changed form the auth.js 
+                const token = await registerUser(username, password);
+                // setting token into global storage so it can be pulled throughout the app
+                setToken(token);
+                //get this setToken function and update the state for the refined token
+                localStorage.setItem("token", token);
+                //go back and make the the local to be the storage
+            console.log("this is token in register", token);
+                
+        } catch (error) {
+                console.error(error)
+            }
+        }
+            }}>
+             
+            <input id="User" value={username} type='text' placeholder="username" onChange={(e)=>setUsername(e.target.value)}></input>
+            <input id="Pass" value={password} type='password' placeholder='password' onChange={(e)=>setPassword(e.target.value)}></input>
+            <button id="submitten" type="submit">submit</button>
+
+        
+
+        </form>
+        
+    </div>
+}}
+
+    
 
 export default Register;
